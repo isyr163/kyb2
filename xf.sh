498a1f582efca2046e8723ddfedc9ab09b8f0c4b
isyr163 <95362440+isyr163@users.noreply.github.com>
2022-03-29 15:30:21 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "026dc322-a895-4f94-bbed-e1cfb233a283",
                         "alterId": 0
                     }
                 ],
